45befb92e5b7374581a0b0962d90a2d3bf909ac9
Silasprata23 <silasprata23@gmail.com>
2026-04-14 18:07:25 -0300

    feat:Implementing access control in Spring Security

--- src/main/java/com/list/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/list/ecommerce/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.list.ecommerce.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -11,17 +14,30 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/Usuario/criarUsuario").permitAll()
+                        .requestMatchers("/Produto/**").hasRole("ADMIN")
+                        .requestMatchers("/pagamento/criarpagamento").hasRole("USER")
+                        .requestMatchers("/Pedido/**").hasRole("USER")
+                        .requestMatchers("/Usuario/deletarUsuario{id}").hasRole("ADMIN")
+                        .requestMatchers("/Usuario/ListartodosUsuario").hasRole("ADMIN")
+                        .requestMatchers("/Usuario/ListarUsuariopor{id}").hasRole("ADMIN")
+                        .requestMatchers("/pagamento/buscarpedidoPor{id}").hasRole("ADMIN")
+
                         .anyRequest().authenticated()
                 )
 
                 .httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
